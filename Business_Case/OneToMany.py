a2f2a9496d5070fe80bd6d7ea850312bfca14782
deushka <ers98ers@gmail.com>
2018-07-24 16:59:52 +0600

    Speed up

--- Business_Case/OneToMany.py
+++ Business_Case/OneToMany.py
@@ -15,10 +15,10 @@ class OneToMany():
         
         # Get a reference to webcam #0 (the default one)
         self.video_capture = cv2.VideoCapture(0)
-        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-        self.video_capture.set(cv2.CAP_PROP_FPS, 100)
-
+        self.video_capture.set(cv2.CAP_PROP_FPS, 30)
+        
         known_face_encodings = []
         known_face_names = []
 
@@ -37,25 +37,26 @@ class OneToMany():
         face_names = []
         process_this_frame = True
         print("Checking...")
-
-        while self.n > 0:
+        while self.n>0:
+            print (self.n)
             # Grab a single frame of video
-            ret, frame = self.video_capture.read()
+       	    ret, frame = self.video_capture.read()
 
             # Resize frame of video to 1/4 size for faster face recognition processing
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
             rgb_small_frame = small_frame[:, :, ::-1]
-
+            face_locations = face_recognition.face_locations(rgb_small_frame)
             # Only process every other frame of video to save time
-            if process_this_frame:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            if self.n%10==0:
+            	face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                face_names = []
-                for face_encoding in face_encodings:
+            	face_names = []
+
+                # Find all the faces and face encodings in the current frame of
+            	for face_encoding in face_encodings:
+                    
                     # See if the face is a match for the known face(s)
                     matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance = 0.43)
                     name = "Unknown"
@@ -66,10 +67,8 @@ class OneToMany():
                         name = known_face_names[first_match_index]
 
                     face_names.append(name)
-
-            process_this_frame = not process_this_frame
-
-
+            	#process_this_frame = not process_this_frame
+            print (self.video_capture.get(cv2.CAP_PROP_FPS))
             # Display the results
                 
             for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -122,17 +121,17 @@ class OneToMany():
                     font = cv2.FONT_HERSHEY_DUPLEX
                     cv2.putText(frame, "NOT RECOGNIZED", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
                     
-                if self.n == 0 and self.accepted == False:
-                    cv2.putText(frame, "BOOOOOOOOOOM", (300, 310), font, 1.0, (255, 255, 255), 1)
-                    status = open("Guests/" + name + "/status.txt", "w")
-                    status.write("Reject")
-                    status.close()
-                    break;
-                else:
-                    if self.accepted == True:
-                        #self.n = 0
-                        break;
-                    self.n -= 1
+            if self.n == 0 and self.accepted == False:
+                cv2.putText(frame, "BOOOOOOOOOOM", (300, 310), font, 1.0, (255, 255, 255), 1)
+                status = open("Guests/" + name + "/status.txt", "w")
+                status.write("Reject")
+                status.close()
+                break;
+            else:
+                if self.accepted == True:
+                    self.n = self.n
+                    #break;
+                self.n -= 1
 
             # Display the resulting image
             cv2.imshow('Video', frame)
