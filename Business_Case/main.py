460f89b4cd4673c91350f6dfeb4cfd2986bfcf97
Faranio <40353497+Faranio@users.noreply.github.com>
2018-07-24 19:56:09 +0600

    OneToMany Algorithm

---
+++ Business_Case/main.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import MainWindow
+import face_recognition
+import cv2
+import time
+
+class ExampleApp(MainWindow.MainWindow):
+
+	def __init__(self, parent = None):
+		super(ExampleApp, self).__init__(parent)
+		self.window = MainWindow.MainWindow()
+
+def main():
+	app = QtWidgets.QApplication(sys.argv)
+	form = ExampleApp()
+	form.setWindowTitle('Face Recognition v 1.0')
+	form.show()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
